21d031fb8a31007ad27026e91394719a7c647d64
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-04-03 20:29:27 +0545

    first stage deployment

--- admin/src/pages/banner/[id].tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EditBannerDialog from "../../components/category/EditBannerDialog";
-
-const Id = () => {
-  return <EditBannerDialog />;
-};
-
-export default Id;

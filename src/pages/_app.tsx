8fa23a88a77d4b24d5dcd90ad82e924e26362911
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-04-04 23:06:53 +0545

    folder structure causing error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import Footer from "../components/Footer";
-import { Provider } from "react-redux";
-import { store } from "../redux/store/store";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 
@@ -14,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <Provider store={store}>
+    <>
       <div className="app_min_height">
         <Navbar />
         <Component {...pageProps} />
@@ -24,7 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         position="top-center"
         reverseOrder={false}
       />
-    </Provider>
+    </>
   );
 }
 
